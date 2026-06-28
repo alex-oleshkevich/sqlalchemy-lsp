9b0ae1777ac8b522d6125f4899fc6d48eaf0a83e
alex.oleshkevich <alex.oleshkevich@gmail.com>
2026-06-28 14:21:28 +0200

    feat: fallback log filter, no-ANSI output, build timestamp in startup log

--- src/main.rs
+++ src/main.rs
@@ -57,14 +57,19 @@ async fn main() {
 
 async fn run_lsp() {
     tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
         .with_writer(std::io::stderr)
+        .with_ansi(false)
         .init();
 
     tracing::info!(
-        "{} v{} starting",
+        "{} v{} (built {}) starting",
         env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_VERSION")
+        env!("CARGO_PKG_VERSION"),
+        env!("BUILD_TIMESTAMP"),
     );
 
     let stdin = tokio::io::stdin();
